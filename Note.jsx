7deb2c07bc14fd5f8afa6e127446736e8397fc54
Ankit Bhatt <48975346+Ankit12bhatt@users.noreply.github.com>
2021-06-07 08:08:32 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>javascript And React,js</h1>
+      <p>
+        
+        Amazing bootcamp by mr.Shaurya Sinha.
+          looking forward for more bootcamps.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
